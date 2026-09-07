eb9a1ab3cfe61cda6f5032fe137680fa65d3d494
Kevin Castillo Reyes <castilloreyeskm@gmail.com>
2026-09-07 15:51:46 -0500

    fix: update vite base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cv-parser/",
+  base: "/",
   server: {
     proxy: {
       "/api/openrouter": {
